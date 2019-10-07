713321fd001e62cf20559596b3fded1510be1b43
K.Yolshin <kostyayolshin@mail.ru>
2019-10-08 00:18:06 +0800

    ❍ Automatic commit by: K.Yolshin

--- src/services/gitServices/committing.js
+++ src/services/gitServices/committing.js
@@ -2,10 +2,8 @@
 import shell from 'shelljs'
 
 export const COMMITTING = {
-
   async commitChanges({ msg }) {
-    const command = 'git commit -m "' +msg +'"'
-    const output = shell.exec(command)
+    const output = shell.exec(`git commit -m "${msg}"`)
     const { stdout, stderr, code } = output
 
     return {

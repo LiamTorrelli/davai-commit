d57744bbf69c7d94b00c500f6ed0e45d00b738da
Kazakov Artem <atleast24.7@list.ru>
2019-10-08 19:17:40 +0800

    ❍ Added a todo ❍
    ☐------------------------------☐
      ✸ Developer: Kazakov Artem
      ✸ Date: Oct 8th [ 19:17:00 ]
    ☐------------------------------☐

--- src/services/gitSubServices/committingSubServices.js
+++ src/services/gitSubServices/committingSubServices.js
@@ -4,6 +4,8 @@ import shell from 'shelljs'
 export const GIT_S_COMMITTING = {
 
   async commitChanges({ commitMessage }) {
+    // TODO: Separate the head from the body of the message
+
     const output = shell.exec(`git commit -m "${commitMessage}"`)
     const { stdout, stderr, code } = output
 

a6f4bd2e0f3cac103e2a973579e9a3f1cbe73566
Kazakov Artem <atleast24.7@list.ru>
2019-10-08 18:00:21 +0800

    ❍ Automatic commit by: Kazakov Artem ☐---------------------------------☐   MODIFIED:  src/config/words.js ☐---------------------------------☐ Generated: Oct 8th [ 18:00:00 ]

--- src/config/words.js
+++ src/config/words.js
@@ -1,28 +1,7 @@
 const tasks = {
-  defaultTask: {
-    title: '# defaultTask. Default task, pls provide description',
-    error: '# defaultTask. Default errored task handler, pls provide description'
-  },
-
   checkIfFilesExist: {
     title: '# Checking file(s). Making sure startup file(s) exist(s)',
     error: '# At least one of the start up files does not exist. Check DAVAI-CONFIG.json'
-  },
-  checkStartUpBranch: {
-    title: '# Checking the branch name.',
-    error: '# You should be on the pre production branch'
-  },
-  mergeMasterBranch: {
-    title: '# Merging master into pre-prod branch',
-    error: '# Merging master into pre-prod branch failed'
-  },
-  checkOpenReleases: {
-    title: '# Checking other open releases',
-    error: '# You should not have other open releases not merged into master'
-  },
-  checkForChanges: {
-    title: '# Checking for the current changes localy',
-    error: '# You should not have any local changes at this point'
   },
   setStatusedFiles: {
     title: '# Setting GIT status files',
@@ -36,73 +15,9 @@ const tasks = {
     title: '# Setting the project info',
     error: '# There was an error while getting project info'
   },
-  createProductionFiles: {
-    title: '# Preparing production files',
-    error: '# There was an error while preparing production files'
-  },
-  updateProductionFiles: {
-    title: '# Updating production files with new version',
-    error: '# There was an error while Updating production files with new version'
-  },
-  updateFilesWithVersionAfterRelease: {
-    title: '# Updating files with new version after release',
-    error: '# There was an error while Updating files with new version after release'
-  },
-  updateFilesWithVersionInNewRelease: {
-    title: '# Updating files with new version in a new release',
-    error: '# There was an error while Updating files with new version in a new release'
-  },
   submitAllToGithub: {
     title: '# Committing',
     error: '# There was an error while Committing'
-  },
-  gitCreateBranch: {
-    title: '# Creating a release branch',
-    error: '# There was an error while creating a release branch'
-  },
-  mergePreProdBranch: {
-    title: '# Merging PRE-PROD branch into a new release branch',
-    error: '# There was an error while Merging PRE-PROD branch into a new release branch'
-  },
-  gitCreateTag: {
-    title: '# Creating release tag',
-    error: '# There was an error while Creating release tag'
-  },
-  handlePushTag: {
-    title: '# Pushing release tag',
-    error: '# There was an error while Pushing release tag'
-  },
-  commitAfterPushingTag: {
-    title: '# Committing after release is done',
-    error: '# There was an error while Committing after release is done'
-  },
-  generateWechatPreview: {
-    title: '# Generate preview in WeChat Devtools',
-    error: '# There was an error while Generating preview in WeChat Devtools'
-  },
-  generateWechatRelease: {
-    title: '# Publishing RELEASE into WeChat',
-    error: '# There was an error while publishing code to wechat server'
-  },
-  loginWechatDevtools: {
-    title: '# Log in WeChat Devtools',
-    error: '# There was an error while Logging in WeChat Devtools'
-  },
-  submitWechatRelease: {
-    title: '# Publishing RELEASE into WeChat',
-    error: '# There was an error while publishing code to wechat server'
-  },
-  deleteProductionFiles: {
-    title: '# Deleting production files',
-    error: '# There was an error while deleting production files'
-  },
-  createGithubCommit: {
-    title: '# Submiting changes to GitHub',
-    error: '# There was an error while Submiting changes to GitHub'
-  },
-  createBuildTag: {
-    title: '# Creating TAG',
-    error: '# There was an error while Creating TAG'
   }
 }
 

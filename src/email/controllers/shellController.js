da0d016c6bd2542ee7477f9124acbbabcf6715c4
Kazakov Artem <atleast24.7@list.ru>
2019-10-11 01:44:21 +0800

    ❍ Automatic commit by: Kazakov Artem
    ☐-------------------------------------------------------------------------------☐
      DELETED:  src/email/REAME.md
      DELETED:  src/email/controllers/filesController.js
      DELETED:  src/email/controllers/htmlController.js
      DELETED:  src/email/controllers/shellController.js
      DELETED:  src/email/dictionary/_translate.js
      DELETED:  src/email/index.js
      DELETED:  src/email/libs/nodemailer/.prettierrc.js
      DELETED:  src/email/libs/nodemailer/CHANGELOG.md
      DELETED:  src/email/libs/nodemailer/CONTRIBUTING.md
      DELETED:  src/email/libs/nodemailer/LICENSE
      DELETED:  src/email/libs/nodemailer/REAME.md
      DELETED:  src/email/libs/nodemailer/lib/addressparser/index.js
      DELETED:  src/email/libs/nodemailer/lib/base64/index.js
      DELETED:  src/email/libs/nodemailer/lib/dkim/index.js
      DELETED:  src/email/libs/nodemailer/lib/dkim/message-parser.js
      DELETED:  src/email/libs/nodemailer/lib/dkim/relaxed-body.js
      DELETED:  src/email/libs/nodemailer/lib/dkim/sign.js
      DELETED:  src/email/libs/nodemailer/lib/fetch/cookies.js
      DELETED:  src/email/libs/nodemailer/lib/fetch/index.js
      DELETED:  src/email/libs/nodemailer/lib/json-transport/index.js
      DELETED:  src/email/libs/nodemailer/lib/mail-composer/index.js
      DELETED:  src/email/libs/nodemailer/lib/mailer/index.js
      DELETED:  src/email/libs/nodemailer/lib/mailer/mail-message.js
      DELETED:  src/email/libs/nodemailer/lib/mime-funcs/index.js
      DELETED:  src/email/libs/nodemailer/lib/mime-funcs/mime-types.js
      DELETED:  src/email/libs/nodemailer/lib/mime-node/index.js
      DELETED:  src/email/libs/nodemailer/lib/mime-node/last-newline.js
      DELETED:  src/email/libs/nodemailer/lib/nodemailer.js
      DELETED:  src/email/libs/nodemailer/lib/qp/index.js
      DELETED:  src/email/libs/nodemailer/lib/sendmail-transport/index.js
      DELETED:  src/email/libs/nodemailer/lib/sendmail-transport/le-unix.js
      DELETED:  src/email/libs/nodemailer/lib/sendmail-transport/le-windows.js
      DELETED:  src/email/libs/nodemailer/lib/ses-transport/index.js
      DELETED:  src/email/libs/nodemailer/lib/shared/index.js
      DELETED:  src/email/libs/nodemailer/lib/smtp-connection/data-stream.js
      DELETED:  src/email/libs/nodemailer/lib/smtp-connection/http-proxy-client.js
      DELETED:  src/email/libs/nodemailer/lib/smtp-connection/index.js
      DELETED:  src/email/libs/nodemailer/lib/smtp-pool/index.js
      DELETED:  src/email/libs/nodemailer/lib/smtp-pool/pool-resource.js
      DELETED:  src/email/libs/nodemailer/lib/smtp-transport/index.js
      DELETED:  src/email/libs/nodemailer/lib/stream-transport/index.js
      DELETED:  src/email/libs/nodemailer/lib/well-known/index.js
      DELETED:  src/email/libs/nodemailer/lib/well-known/services.json
      DELETED:  src/email/libs/nodemailer/lib/xoauth2/index.js
      DELETED:  src/email/libs/nodemailer/package.json
      DELETED:  src/email/modules/release.js
      DELETED:  src/email/modules/singleCommit.js
      DELETED:  src/email/services/sender.js
      DELETED:  src/email/utils/helpers.js
      DELETED:  src/email/utils/logger.js
    ☐-------------------------------------------------------------------------------☐
    Generated: Oct 11th [ 01:44:00 ]

--- src/email/controllers/shellController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const getPrametersFromShell = () => process.argv.filter((value, index) => (index !== 0 && index !== 1) ? value : '')
-
-module.exports = {
-  getPrametersFromShell
-}
